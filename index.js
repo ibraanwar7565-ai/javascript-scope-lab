37cbfbdd5697b8a39383e5700f1cc6a75045c65a
Abdirahim Abdikadir Ibrahim <ibraanwar7565@gmail.com>
2026-04-17 10:12:30 +0300

    Completed Flatburger scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+// Global scope - array with hamburger and cheeseburger
+var burgerMenu = ["hamburger", "cheeseburger"];
+
+// Global scope - featured drink string
+var featuredDrink = "chocolate milkshake";
+
+// Function scope - getFeaturedMilkshake
+function getFeaturedMilkshake() {
+  var featuredMilkshake = featuredDrink;
+  featuredMilkshake = "strawberry milkshake";
+  return featuredMilkshake;
+}
+
+// Block scope - addBurger
+function addBurger() {
+  if (true) {
+    let newBurger = "veggie burger";
+    burgerMenu.push(newBurger);
+  }
+  return burgerMenu;
+}
